1f460b517d49833a7f97b15261d12336d4e83aa9
Wael Al-Bakry <waelbk96@gmail.com>
2026-02-19 00:17:43 +0200

    Roll Back

--- C#_Project_E-commerce_App/obj/Debug/net10.0/C#_Project_E-commerce_App.AssemblyInfo.cs
+++ C#_Project_E-commerce_App/obj/Debug/net10.0/C#_Project_E-commerce_App.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C#_Project_E-commerce_App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6d42b57d3bb17aca584475352cf5bcbdd814309")]
 [assembly: System.Reflection.AssemblyProductAttribute("C#_Project_E-commerce_App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C#_Project_E-commerce_App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
