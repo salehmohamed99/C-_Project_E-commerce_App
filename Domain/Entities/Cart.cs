485492db88c03cef2ceafafaeed482d01009234d
salehmohamed99 <95438192+salehmohamed99@users.noreply.github.com>
2026-02-19 13:31:46 +0200

    add base , cart and user entity

---
+++ Domain/Entities/Cart.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Domain.Models;
+
+namespace Domain.Entities
+{
+    public class Cart : BaseEntity<int>
+    {
+        public decimal TotalPrice { get; set; } = 0;
+
+        public int? UserID { get; set; }
+        public User User { get; set; }
+    }
+}
---
+++ Domain/Entities/User.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Domain.Entities
+{
+    public enum Role
+    {
+        Admin,
+        Customer,
+    }
+
+    [Index(nameof(Email), IsUnique = true)]
+    [Index(nameof(UserName), IsUnique = true)]
+    public class User : BaseEntity<int>
+    {
+        public string Name { get; set; } = string.Empty;
+        public string UserName { get; set; } = string.Empty;
+
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+        public string ProfilePicture { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public DateTime DateOfBirth { get; set; }
+        public Role Role { get; set; } = Role.Customer;
+
+        public int CartID { get; set; }
+        public Cart Cart { get; set; }
+    }
+}
