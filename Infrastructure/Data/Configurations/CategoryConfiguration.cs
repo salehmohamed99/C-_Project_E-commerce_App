e9cbf2d9529ac9ab7138fee1f3d62a831119cb9d
rwida <rwidajaber@gmail.com>
2026-02-20 15:07:19 +0200

    Add Category configuration

---
+++ Infrastructure/Data/Configurations/CategoryConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Domain.Entities;
+using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Data.Configurations
+{
+    public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasKey(c => c.ID);
+
+            builder.Property(c => c.Name)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder.HasMany(c => c.products)
+                .WithOne(p => p.category)
+                .HasForeignKey(p => p.CategoryId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
