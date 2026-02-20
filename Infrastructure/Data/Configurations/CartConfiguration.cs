7b96e6e3d5a60e5f24661d6da8014c2817ce4b86
salehmohamed99 <95438192+salehmohamed99@users.noreply.github.com>
2026-02-20 16:07:27 +0200

    add CartConfiguration

---
+++ Infrastructure/Data/Configurations/CartConfiguration.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Data.Configurations
+{
+    public class CartConfiguration : IEntityTypeConfiguration<Cart>
+    {
+        public void Configure(EntityTypeBuilder<Cart> builder)
+        {
+            builder.HasKey(c => c.ID);
+
+            builder.Property(c => c.ID).HasColumnName("Cart_id");
+
+            builder
+                .Property(c => c.TotalPrice)
+                .IsRequired()
+                .HasColumnType("decimal(10,2)")
+                .HasDefaultValue(0m);
+
+            builder
+                .Property(c => c.CreatedAt)
+                .HasColumnName("Created_At")
+                .IsRequired()
+                .HasDefaultValueSql("GETUTCDATE()");
+
+            builder
+                .Property(c => c.UpdatedAt)
+                .HasColumnName("Updated_At")
+                .IsRequired()
+                .HasDefaultValueSql("GETUTCDATE()");
+
+            builder.Property(c => c.UserID).HasColumnName("User_id");
+
+            builder.HasIndex(c => c.UserID).IsUnique();
+
+            builder
+                .HasOne(c => c.User)
+                .WithOne(u => u.Cart)
+                .HasForeignKey<Cart>(c => c.UserID)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
