1d36046d0132420ba218e8d360f7c03e20beaf0f
rwida <rwidajaber@gmail.com>
2026-02-20 13:00:47 +0200

    Add Category entity

---
+++ Domain/Entities/Category.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Entities
+{
+    public class Category: BaseEntity<int>
+    {
+        public string Name { get; set; }
+    }
+}
