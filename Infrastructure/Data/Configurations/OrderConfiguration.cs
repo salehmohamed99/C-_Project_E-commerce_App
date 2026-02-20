2ec265ff417d0b39e898c9d0964de82ce3d574e0
Nada Osama <enada1349@gmail.com>
2026-02-20 17:08:01 +0200

    Add order configuration

---
+++ Infrastructure/Data/Configurations/OrderConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Data.Configurations
+{
+    public class OrderConfiguration : IEntityTypeConfiguration<Order>
+    {
+        public void Configure(EntityTypeBuilder<Order> builder)
+        {
+            builder.HasKey(o => o.ID);
+
+            builder.Property(o => o.ShippingAddress)
+                .IsRequired()
+                .HasMaxLength(250);
+
+            builder.HasOne(o => o.User)
+                .WithMany(u => u.Orders)
+                .HasForeignKey(o => o.UserId);
+        }
+    }
+}
---
+++ Infrastructure/Data/Configurations/OrderProductConfiguartion.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Data.Configurations
+{
+    public class OrderProductConfiguartion : IEntityTypeConfiguration<OrderProduct>
+    {
+        public void Configure(EntityTypeBuilder<OrderProduct> builder)
+        {
+            builder.HasKey(op => op.ID);
+
+            builder.Property(op => op.Quantity)
+                .IsRequired();
+
+            builder.Property(op => op.UnitPrice)
+                .HasColumnType("decimal(18,2)");
+
+            builder.HasOne(op => op.order)
+                .WithMany(o => o.OrderProducts)
+                .HasForeignKey(op => op.OrderId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasOne(op => op.product)
+                .WithMany(p => p.OrderProducts)
+                .HasForeignKey(op => op.ProductId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
