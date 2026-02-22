d633384f35fdd78abaa4fa5df94939a1dab1b5eb
salehmohamed99 <95438192+salehmohamed99@users.noreply.github.com>
2026-02-22 22:55:48 +0200

    Add CartDTO class for user shopping cart data
    Introduced CartDTO in Application.DTOs to represent a user's shopping cart, including UserID, TotalPrice, and a list of CartItem objects.

---
+++ Application/DTOs/CartDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Domain.Entities;
+
+namespace Application.DTOs
+{
+    public class CartDTO
+    {
+        public int UserID { get; set; }
+        public decimal TotalPrice { get; set; } = 0;
+        public List<CartItem> CartItems { get; set; }
+    }
+}
