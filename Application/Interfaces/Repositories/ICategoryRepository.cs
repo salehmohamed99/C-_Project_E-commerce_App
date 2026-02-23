b744387f49fac7da67e4897f9f96c51ff2f40954
rwida <rwidajaber@gmail.com>
2026-02-23 21:27:08 +0200

    Add CategoryRepository and ICategoryRepository interface

---
+++ Application/Interfaces/Repositories/ICategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Interfaces.Repositories
+{
+    public interface ICategoryRepository:IGenericRepository<Category,int>
+    {
+        Category GetByName(string name);
+        bool HasProducts(int categoryId);
+    }
+}
---
+++ Infrastructure/Repositories/CategoryRepository.cs
@@ -0,0 +1,53 @@
+﻿using Application.DTOs.CategoryDTOs;
+using Application.Interfaces.Repositories;
+using Domain.Entities;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Repositories
+{
+    public class CategoryRepository
+     : GenericRepository<Category, int>, ICategoryRepository
+    {
+        public CategoryRepository(ApplicationDbContext context)
+            : base(context)
+        {
+        }
+
+        public IQueryable<Category> GetCategoriesWithProducts()
+        {
+            return _context.Categories
+                           .Include(c => c.products);
+
+
+        }
+
+        public async Task<Category> GetByNameAsync(string name)
+        {
+            return await _context.Categories
+                                 .Include(c => c.products)
+                                 .AsNoTracking()
+                                 .FirstOrDefaultAsync(c => c.Name == name);
+        }
+
+        public async Task<bool> HasProductsAsync(int categoryId)
+        {
+            return await _context.Products
+                                 .AnyAsync(p => p.CategoryId == categoryId);
+        }
+
+        public Category GetByName(string name)
+        {
+            return _context.Categories.
+                FirstOrDefault(c => c.Name == name);
+        }
+
+        public bool HasProducts(int categoryId)
+        {
+            return _context.Products.Any(p => p.CategoryId == categoryId);
+        }
+    }
+}
