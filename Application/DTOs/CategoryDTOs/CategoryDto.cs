8bc688012a0c7229d11d31710a3e81a8bba95264
rwida <rwidajaber@gmail.com>
2026-02-23 22:24:13 +0200

    Add Category mapper

--- Application/DTOs/CategoryDTOs/CategoryDto.cs
+++ Application/DTOs/CategoryDTOs/CategoryDto.cs
@@ -6,7 +6,7 @@ namespace Application.DTOs.CategoryDTOs
 {
     public class CategoryDto
     {
-        //public int Id { get; set; }
+        public int Id { get; set; }
         public string Name { get; set; }
         public int ProductCount { get; set; }       
         public DateTime Created_At { get; set; }
---
+++ Application/Mappers/Mapper.cs
@@ -0,0 +1,21 @@
+﻿using Application.DTOs.CategoryDTOs;
+using Domain.Entities;
+using Mapster;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Mappers
+{
+    public class Mapper
+    {
+        public void Map()
+        {
+           
+            TypeAdapterConfig<Category, CategoryDto>.NewConfig();
+            TypeAdapterConfig<CreateCategoryDto, Category>.NewConfig();
+            TypeAdapterConfig<Category, UpdateCategoryDto>.NewConfig();
+                
+        }
+    }
+}
