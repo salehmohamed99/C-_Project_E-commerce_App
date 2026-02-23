55f3ce7d3dfba03e5f56db4114c4d62303f0d251
Wael Al-Bakry <waelbk96@gmail.com>
2026-02-23 21:45:05 +0200

    Add Id property to CartItemDto for unique identification
    Added an int Id property to the CartItemDto class in the Application.DTOs.CartItemDto namespace. This allows each cart item to be uniquely identified, which can improve data handling and referencing within the application.

--- Application/DTOs/CartItemDto/CartItemDto.cs
+++ Application/DTOs/CartItemDto/CartItemDto.cs
@@ -6,6 +6,7 @@ namespace Application.DTOs.CartItemDto
 {
     public class CartItemDto
     {
+        public int Id { get; set; }
         public int CartId { get; set; }
         public int ProductId { get; set; }
         public int Quantity { get; set; }
