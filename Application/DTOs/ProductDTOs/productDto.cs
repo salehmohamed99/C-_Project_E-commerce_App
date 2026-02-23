aad90a8d833e6a39ff6bcea1594ec2b9b02f1098
rwida <rwidajaber@gmail.com>
2026-02-24 00:45:28 +0200

    Add product DTOs

---
+++ Application/DTOs/ProductDTOs/productDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.DTOs.ProductDTOs
+{
+    public class productDTO
+    {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string Image { get; set; }
+            public decimal Price { get; set; }
+            public string Description { get; set; }
+            public int Units_In_Stock { get; set; }
+            public bool IsActive { get; set; }
+            public int CategoryId { get; set; }
+            public string CategoryName { get; set; }
+            public DateTime Created_At { get; set; }
+            public DateTime Updated_At { get; set; }
+        
+    }
+}
