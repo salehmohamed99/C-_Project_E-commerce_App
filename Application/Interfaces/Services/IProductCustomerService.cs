ef986df749a0119195420a88fd69fa97453a52e8
rwida <rwidajaber@gmail.com>
2026-02-24 20:38:43 +0200

    Add ProductCustomerService

---
+++ Application/Interfaces/Services/IProductCustomerService.cs
@@ -0,0 +1,15 @@
+﻿using Application.DTOs.ProductDTOs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Interfaces.Services
+{
+    public interface IProductCustomerService
+    {
+        IQueryable<ProductDto> GetAll();              
+        ProductDto GetById(int id);                   
+       
+        IEnumerable<ProductDto> SearchByName(string name);
+    }
+}
